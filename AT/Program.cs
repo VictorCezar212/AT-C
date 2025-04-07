04b3ec309d1e0919dcd5156097903149fb5d2350
Victor Cezar <115908435+VictorCezar212@users.noreply.github.com>
2025-04-07 03:20:25 -0300

    Add files via upload

---
+++ AT/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá,meu nome é Victor!");
+            Console.WriteLine("Nasci em 15/07/2002 e estou aprendendo C#!");
+        }
+    }
+}
---
+++ AT02/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AT02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome completo:");
+            string nomeOriginal = Console.ReadLine();
+
+            string nomeCifrado = CifrarNome(nomeOriginal, 2);
+
+            Console.WriteLine("Nome original: " + nomeOriginal);
+            Console.WriteLine("Nome cifrado: " + nomeCifrado);
+        }
+
+        static string CifrarNome(string nome, int deslocamento)
+        {
+            char[] caracteres = nome.ToCharArray();
+
+            for (int i = 0; i < caracteres.Length; i++)
+            {
+                char c = caracteres[i];
+
+                if (!char.IsLetter(c))
+                {
+                    continue;
+                }
+
+                char baseChar = char.IsUpper(c) ? 'A' : 'a';
+                c = (char)(baseChar + (c - baseChar + deslocamento) % 26);
+                caracteres[i] = c;
+            }
+
+            return new string(caracteres);
+        }
+    }
+}
